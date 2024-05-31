0c6fca112b8f4c00162405882a6bdf6553a6e85b
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-05-31 18:36:57 +0600

    aggregate function work on going

--- index.js
+++ index.js
@@ -256,6 +256,53 @@ async function run() {
         revenue,
       });
     });
+    // order status
+    /**
+     * ------------------------
+     * NON-Efficient Way
+     * -----------------------
+     * 1. Load all the payments
+     * 2. For every menuItemIds (which is an array), go find the item from menu collection.
+     * 3. For every item in the menu collection that you found from a payment  entry (document)
+     * */
+    // using aggregate pipeline
+    app.get("/order-stats", async (req, res) => {
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $unwind: "$menuItemIds",
+          },
+          {
+            $lookup: {
+              from: "menu",
+              localField: "menuItemIds",
+              foreignField: "_id",
+              as: "menuItems",
+            },
+          },
+          {
+            $unwind: "$menuItems",
+          },
+          {
+            $group: {
+              _id: "$menuItems.category",
+              quantity: { $sum: 1 },
+              revenue: { $sum: "$menuItems.price" },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              category: "$_id",
+              quantity: "$quantity",
+              revenue: "$revenue",
+            },
+          },
+        ])
+        .toArray();
+
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
